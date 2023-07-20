48089b916e9af144861291dc71f67c4a982c84be
shun916 <118881278+code-polaris044@users.noreply.github.com>
2023-07-21 00:12:58 +0900

    ルーター

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 import express from "express";
+const app = express();
+
+const PORT = 3100;
+
+app.get("/", (req, res) => {
+  res.send("Hello express");
+});
+
+app.listen(PORT, () => {
+  console.log("サーバー起動中");
+});
